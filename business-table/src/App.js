a0c4b6c235ee6614e9306497f17e44158b68fd63
rupomsoft <rupomplaystore@gmail.com>
2021-09-11 20:29:18 +0600

    table started
    table started

---
+++ business-table/src/App.js
@@ -0,0 +1,11 @@
+import React, {Fragment} from 'react';
+import BusinessTable from "./pages/BusinessTable";
+const App = () => {
+    return (
+        <Fragment>
+            <BusinessTable/>
+        </Fragment>
+    );
+};
+
+export default App;
---
+++ business-table/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import  './assets/css/bootstrap.min.css';
+import  './assets/css/animate.min.css';
+import reportWebVitals from './reportWebVitals';
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+reportWebVitals();
