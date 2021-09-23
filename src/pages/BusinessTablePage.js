9db612f04fdcd1175a65d529782b7742a79d94d8
Rabbil Hasan <33941621+rupomsoft@users.noreply.github.com>
2021-09-23 11:56:08 +0600

    import porblem fix import porblem fix

--- src/pages/BusinessTablePage.js
+++ src/pages/BusinessTablePage.js
@@ -1,32 +1,24 @@
-import React, {Fragment, useEffect, useState} from 'react';
+import React, { Fragment, useEffect, useState } from "react";
 import axios from "axios";
-import BusinessTable from "../component/BusinessTable";
-import {Container} from "react-bootstrap";
+import BusinessTable from "../components/BusinessTable";
 
 const BusinessTablePage = () => {
+  const [ItemList, SetItemList] = useState([]);
+  useEffect(() => {
+    axios.get("https://jsonplaceholder.typicode.com/todos").then((res) => {
+      SetItemList(res.data);
+    });
+  }, []);
 
-    const [ItemList,SetItemList]=useState([])
-    useEffect(()=>{
-        axios.get("https://jsonplaceholder.typicode.com/todos")
-            .then((res)=>{
-                SetItemList(res.data)
-            })
-    },[])
-
-    if(ItemList.length===0){
-        return(
-            <Fragment>
-
-            </Fragment>
-        )
-    }
-    else {
-        return(
-            <Fragment>
-                <BusinessTable ItemList={ItemList}/>
-            </Fragment>
-        )
-    }
+  if (ItemList.length === 0) {
+    return <Fragment></Fragment>;
+  } else {
+    return (
+      <Fragment>
+        <BusinessTable ItemList={ItemList} />
+      </Fragment>
+    );
+  }
 };
 
 export default BusinessTablePage;
